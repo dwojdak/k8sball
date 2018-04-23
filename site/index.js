4582806efa534a0897c0ed43c7b9fb6a2f312d03
Damon Wojdak <dwojdak@gmail.com>
2018-04-22 23:02:16 -0700

    update assets

--- site/index.js
+++ site/index.js
@@ -1,8 +1,7 @@
 $(document).ready(function(){
 
   var magic8Ball = {};
-  magic8Ball.listofanswers = ["kubectl delete namespace", "Tweet @kelseyhightower for help.", "Check github issues.", "It's Docker.", "kubectl cordon ${NODE}", "CrashLoopBackoff, ask question again later.", "Firewalled.", "Throw yourself at the mercy of SIG on Slack.", "Yes.", "Signs point to kubectl get po.", "Reply hazy, try kubectl describe all.", "Ask again later, ImagePullBackOff", "Concentrate and kubectl get events.", "Don't count on it.", "My reply is node is not schedulable.", "My sources say node is tainted.", "Outlook not so healthz.", "Very doubtful. Upgrade cluster to latest version."];
-
+  magic8Ball.listofanswers = [ "kubectl delete namespace", "Tweet Kelsey Hightower for help", "Check github issues", "It's Docker", "Timeout expired waiting for volumes to attach", "kubectl cordon $NODE", "Crash Loop Backoff", "Firewalled", "Throw yourself at the mercy of SIG on Slack", "Yes", "Signs point to kubectl get po", "Reply hazy, try kubectl describe all", "Ask again later: Image Pull BackOff", "Concentrate... Now kubectl get events", "Docs are wrong", "Reply not schedulable", "My sources say the node is tainted", "Outlook not healthz", "Apply more labels", "Very doubtful. Upgrade cluster to latest version." ];
   magic8Ball.getAnswer = function()
   {
     var randomNumber = Math.random();
